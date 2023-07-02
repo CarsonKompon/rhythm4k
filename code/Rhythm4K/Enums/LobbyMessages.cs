721235d17063181437425cebf6f26c279d3e3f77
Carson Kompon <carsokompo@gmail.com>
2023-07-02 10:56:21 -0400

    removing core code

--- code/Rhythm4K/Enums/LobbyMessages.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Rhythm4K;
-
-public enum LobbyMessages
-{
-    UPDATE_SONG,
-    START_GAME,
-    UPDATE_DIFFICULTY
-}
--- code/Rhythm4K/Enums/NoteType.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Rhythm4K;
-
-public enum NoteType
-{
-    Normal = 0,
-    Mine = 1,
-    Hold = 999,
-}
